60f8d02ae1a98f167c37ae32490fba785032e16c
Ankur Semle <semleankur@gmail.com>
2024-03-14 16:05:01 +0530

    calling weather api

--- src/components/BlackBarsFrame.js
+++ src/components/BlackBarsFrame.js
@@ -1,25 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
 
-const BlackBarsFrame = () => {
-  const [weatherData, setWeatherData] = useState(null);
-  const latitude = 33.44;
-  const longitude = -94.04;
-  const api_key = process.env.WEATHER_API_KEY;
+const BlackBarsFrame = ({weatherData}) => {
+ 
 
-  useEffect(() => {
-    // Fetch weather data from OpenWeatherMap API
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=d0abc0ba8005c0892c07ca74d08e9947`)
-      .then(response => response.json())
-      .then(data => {
-        // Set the weather data in the state
-        console.log(weatherData);
-        setWeatherData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-      });
-  }, []); // Run the effect only once when the component mounts
+
   return (
     <section className="self-stretch flex flex-col items-start justify-start gap-[10px_0px] max-w-full shrink-0 text-left text-6xl text-darkslategray font-abhaya-libre">
       <header className="self-stretch flex flex-row items-start justify-start max-w-full text-center text-sm text-black font-abhaya-libre">
@@ -27,31 +10,16 @@ const BlackBarsFrame = () => {
           <div className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px]" />
           <div className="w-[54px] flex flex-row items-start justify-start pt-[3px] px-0 pb-px box-border z-[1]">
             <div className="flex-1 relative">
-              <span className="tracking-[-0.28px]">9:4</span>1
+              <span className="tracking-[-0.28px]">9:41</span>
             </div>
           </div>
           <div className="w-[66.6px] flex flex-col items-start justify-start pt-[4.300000000000011px] px-0 pb-0 box-border">
             <div className="self-stretch flex flex-row items-start justify-start gap-[0px_5px]">
-              <img
-                className="h-[10.7px] w-[17px] relative z-[1]"
-                loading="lazy"
-                alt=""
-                src="/cellular-connection.svg"
-              />
-              <img
-                className="h-[11px] w-[15.3px] relative z-[1]"
-                loading="lazy"
-                alt=""
-                src="/wifi.svg"
-              />
+              <img className="h-[10.7px] w-[17px] relative z-[1]" loading="lazy" alt="" src="/cellular-connection.svg" />
+              <img className="h-[11px] w-[15.3px] relative z-[1]" loading="lazy" alt="" src="/wifi.svg" />
               <div className="h-[11.3px] w-[24.3px] relative z-[1]">
                 <div className="absolute top-[0px] right-[2.3px] rounded-[2.67px] box-border w-[22px] h-[11.3px] opacity-[0.35] mix-blend-normal border-[1px] border-solid border-black" />
-                <img
-                  className="absolute top-[3.7px] right-[0px] w-[1.3px] h-1 mix-blend-normal"
-                  loading="lazy"
-                  alt=""
-                  src="/cap.svg"
-                />
+                <img className="absolute top-[3.7px] right-[0px] w-[1.3px] h-1 mix-blend-normal" loading="lazy" alt="" src="/cap.svg" />
                 <div className="absolute top-[2px] right-[4.3px] rounded-[1.33px] bg-black w-[18px] h-[7.3px] z-[1]" />
               </div>
             </div>
@@ -62,29 +30,19 @@ const BlackBarsFrame = () => {
         <div className="flex-1 flex flex-row items-end justify-center gap-[0px_52px] max-w-full">
           <div className="flex-1 flex flex-col items-start justify-start pt-0 px-0 pb-0.5 box-border min-w-[93px]">
             <div className="self-stretch h-[154px] flex flex-col items-start justify-start relative gap-[35px_0px]">
-              <img
-                className="w-[18px] h-2 relative object-contain"
-                loading="lazy"
-                alt=""
-                src="/settingsliderhorizontal-copy@2x.png"
-              />
+              <img className="w-[18px] h-2 relative object-contain" loading="lazy" alt="" src="/settingsliderhorizontal-copy@2x.png" />
               <div className="!m-[0] absolute top-[43px] left-[0px] gap-[6px_0px] w-full grid">
                 <h2 className="m-0 text-inherit tracking-[0.25px] font-bold font-inherit">
-                  <p className="m-0">New York,</p>
-                  <p className="m-0">United States</p>
+                  <p className="m-0">{weatherData.city},</p>
+                  <p className="m-0">{weatherData.country}</p>
                 </h2>
                 <div className="flex flex-col items-start justify-start gap-[10px_0px] text-mini text-darkgray">
-                  <b className="relative">Oct 04, 2019</b>
+                  <b className="relative">{weatherData.date}</b>
                   <div className="flex flex-row items-start justify-start py-0 px-px text-royalblue">
                     <div className="flex flex-row items-start justify-start gap-[0px_7.7px]">
-                      <img
-                        className="h-[17.5px] w-[20.3px] relative"
-                        loading="lazy"
-                        alt=""
-                        src="/weatherclouds.svg"
-                      />
+                      <img className="h-[17.5px] w-[20.3px] relative" loading="lazy" alt="" src={`/weather/${weatherData.weather.icon}.svg`} />
                       <div className="flex flex-col items-start justify-start pt-px px-0 pb-0">
-                        <b className="relative">Cloudy</b>
+                        <b className="relative">{weatherData.weather.description}</b>
                       </div>
                     </div>
                   </div>
@@ -94,20 +52,10 @@ const BlackBarsFrame = () => {
           </div>
           <div className="h-[164px] w-[130px] flex flex-col items-end justify-start gap-[38.2px_0px] min-w-[130px] text-2xs text-white">
             <div className="flex flex-row items-start justify-start py-0 px-[5px]">
-              <img
-                className="h-[15.8px] w-[15.8px] relative"
-                loading="lazy"
-                alt=""
-                src="/realestatesearchhouse.svg"
-              />
+              <img className="h-[15.8px] w-[15.8px] relative" loading="lazy" alt="" src="/realestatesearchhouse.svg" />
             </div>
             <div className="self-stretch flex-1 flex flex-row items-end justify-end p-[9px] relative">
-              <img
-                className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full object-cover"
-                loading="lazy"
-                alt=""
-                src="/bitmap@2x.png"
-              />
+              <img className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full object-cover" loading="lazy" alt="" src="/bitmap@2x.png" />
               <div className="rounded-md bg-crimson overflow-hidden flex flex-row items-start justify-start pt-1.5 pb-[3px] pr-3 pl-[9px] z-[1]">
                 <div className="h-[22px] w-[43px] relative rounded-md bg-crimson hidden" />
                 <b className="relative tracking-[0.11px] z-[1]">LIVE</b>
--- src/pages/IPhoneMini.js
+++ src/pages/IPhoneMini.js
@@ -1,19 +1,62 @@
 import BlackBarsFrame from "../components/BlackBarsFrame";
 import BasicShapeText from "../components/BasicShapeText";
 import FrameComponent from "../components/FrameComponent";
+import { useEffect, useState } from "react";
+
 
 const IPhoneMini = () => {
+
+  const [weatherData, setWeatherData] = useState({
+    city: "",
+    country: "",
+    date: "",
+    weather: {
+      icon: "",
+      description: "",
+    },
+  });
+  
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=22a64c7875a8483aab24ae8b693f99dc`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch weather data");
+        }
+        const data = await response.json();
+        console.log(data);
+        // Update weatherData state with API response
+        setWeatherData({
+          city: data.name,
+          country: data.sys.country,
+          date: new Date(data.dt * 1000).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }),
+          weather: {
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          },
+        });
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeatherData();
+
+    return () => {
+      // Cleanup function if needed
+    };
+  }, []);
   return (
     <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start gap-[48px_0px] tracking-[normal]">
       <div className="self-stretch h-[812px] relative bg-white hidden z-[0]" />
-      <BlackBarsFrame />
+      <BlackBarsFrame weatherData={weatherData}/>
       <img
         className="w-full h-[159px] absolute !m-[0] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden shrink-0"
         alt=""
         src="/group-6.svg"
       />
-      <BasicShapeText />
-      <FrameComponent />
+      <BasicShapeText weatherData={weatherData}/>
+      <FrameComponent weatherData={weatherData}/>
       <footer className="self-stretch flex flex-row items-start justify-center pt-5 pb-[9px] pr-5 pl-[21px] shrink-0 z-[1]">
         <div className="h-[5px] w-[134px] relative rounded-81xl bg-black" />
       </footer>
